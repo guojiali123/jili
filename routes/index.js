dc254dccbe816732e42da37fce6d1fcbec5ebb1f
guojiali123 <1762577235@qq.com>
2017-10-25 19:36:32 +0800

    作业
    1025

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var fs=require("fs");
+/* GET home page. */
+router.post('/gjl', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin","*");
+  var temp=[];
+	if(req.body.abc==1){
+		temp=[1,2,3]
+	}else if(req.body.abc==2){
+		temp=[4,5,6]
+	}
+  res.send({"name":temp})
+});
+
+module.exports = router;
